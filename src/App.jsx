fa2685d979351c4df4d7074d8445b2e53bece606
Gustavo Marques <gustavo-markes@hotmail.com>
2023-12-18 10:43:06 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Perfil from "./components/Perfil";
+import Formulario from "./components/Formulario";
+import ReposList from "./components/ReposList";
+import styles from "./MainInput.module.css";
+
+function App() {
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState("");
+
+  return (
+    <>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.search}
+          type="text"
+          onBlur={(e) => setNomeUsuario(e.target.value)}
+        />
+      </div>
+
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
+      {/* {formularioEstaVisivel && <Formulario />}
+      <button
+        onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)}
+        type="button"
+      >
+        Toggle form
+      </button> */}
+    </>
+  );
+}
+
+export default App;
